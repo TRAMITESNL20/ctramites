274060587f0e23df00cbc97083b21a2b057c0af1
Adriana1705 <adriana.contreras@futurite.com>
2020-08-28 17:10:59 -0500

    migracion y repos tramites

---
+++ app/Entities/TramitesCampoTramite.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+use Prettus\Repository\Contracts\Transformable;
+use Prettus\Repository\Traits\TransformableTrait;
+
+/**
+ * Class TramitesCampoTramite.
+ *
+ * @package namespace App\Entities;
+ */
+class TramitesCampoTramite extends Model implements Transformable
+{
+    use TransformableTrait;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+     protected $fillable = [
+        'id',
+        'id_tramite',
+        'id_campo',
+        'id_tipo',
+        'caracteristicas'
+      ];
+
+      protected $table = "tramites_campo_tramites";
+
+}
---
+++ app/Entities/TramitesCampos.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+use Prettus\Repository\Contracts\Transformable;
+use Prettus\Repository\Traits\TransformableTrait;
+
+/**
+ * Class TramitesCampos.
+ *
+ * @package namespace App\Entities;
+ */
+class TramitesCampos extends Model implements Transformable
+{
+    use TransformableTrait;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+      'id',
+       'descripcion'
+    ];
+
+    protected $table = "tramites_campos";
+}
