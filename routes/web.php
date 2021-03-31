4a9eaaa4b9ed8b10e38fc67daaf0ec5af85a3270
Raymundo Salazar <raymundo.salser@hotmail.com>
2021-03-31 10:31:34 -0600

    add to dd endpoint

--- routes/web.php
+++ routes/web.php
@@ -65,7 +65,7 @@ Route::group(["prefix" => getenv("APP_PREFIX") ?? "/"], function(){
 		$client->setCredentials($usr, $pass);
         $response = $client->call("NotificarPago", $data);
 
-		return dd($data, $response);
+		return dd($data, $wsdl, $response);
 	});
 
 	Route::get("/formato-declaracion/{id}", "FormatoDeclaracionController@index");
