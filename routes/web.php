4d3cd27a58e67ab438c5cc5170694bb87b0f3071
Raymundo Salazar <raymundo.salser@hotmail.com>
2021-03-31 13:10:45 -0600

    test SoapClient

--- routes/web.php
+++ routes/web.php
@@ -67,6 +67,47 @@ Route::group(["prefix" => getenv("APP_PREFIX") ?? "/"], function(){
 			'Password' => $pass
 		);
 
+		$request = '<soapenv:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:urn="urn:egobws">
+		   <soapenv:Header/>
+		   <soapenv:Body>
+		      <urn:ConsultaTransaccion soapenv:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+		         <trans xsi:type="egob:transT1" xmlns:egob="http://localhost/egobierno/">
+		            <!--You may enter the following 7 items in any order-->
+		            <reference xsi:type="xsd:string">'.$data['reference'].'</reference>
+		            <bank xsi:type="xsd:string">'.$data['bank'].'</bank>
+		            <date xsi:type="xsd:string">'.$data['date'].'</date>
+		            <string xsi:type="xsd:string">'.$data['string'].'</string>
+		            <user xsi:type="xsd:string">'.$data['user'].'</user>
+		            <password xsi:type="xsd:string">'.$data['password'].'</password>
+		            <branch xsi:type="xsd:string">'.$data['branch'].'</branch>
+		         </trans>
+		      </urn:ConsultaTransaccion>
+		   </soapenv:Body>
+		</soapenv:Envelope>
+		';
+
+		$header = [
+			'Content-type: text/xml;charset="utf-8"',
+			'Accept: text/xml',
+			'Cache-Control: no-cache',
+		];
+
+		$ch = curl_init();
+
+		curl_setopt($ch, CURLOPT_URL, $wsdl);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_POST, true);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $request);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+
+		$result = curl_exec($ch);
+		curl_close($ch);
+		$response = preg_replace("/(<\/?)(\w+):([^>]*>)/", "$1$2$3", $result);
+		$xml = new SimpleXMLElement($response);
+		$body = $xml->xpath('//soapBody')[0];
+		$array = json_decode(json_encode((array)$body), TRUE);
+		dd($array);
+
 		// $request_json= array(
 		// 	'token' => $token,
 		// 	'importe_transaccion' =>$sumMonto,
@@ -76,15 +117,15 @@ Route::group(["prefix" => getenv("APP_PREFIX") ?? "/"], function(){
 		// 	'tramite' =>$tramite
 		// );
 
-		$data = [
-			"reference" => "119974116120000246840330687248",
-			"bank" => "LOCAL",
-			"date" => "2021-03-31",
-			"string" => "uno",
-			"user" => "LOCAL",
-			"password" => "LOCAL",
-			"branch" => "123"
-		];
+		// $data = [
+		// 	"reference" => "119974116120000246840330687248",
+		// 	"bank" => "LOCAL",
+		// 	"date" => "2021-03-31",
+		// 	"string" => "uno",
+		// 	"user" => "LOCAL",
+		// 	"password" => "LOCAL",
+		// 	"branch" => "123"
+		// ];
 
 		// $repuesta;
 		// $datos;
