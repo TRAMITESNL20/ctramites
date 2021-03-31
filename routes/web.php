edff33f0181239c046f4f63ada638af05977dd74
Raymundo Salazar <raymundo.salser@hotmail.com>
2021-03-31 10:51:49 -0600

    test with type

--- routes/web.php
+++ routes/web.php
@@ -47,9 +47,9 @@ Route::group(["prefix" => getenv("APP_PREFIX") ?? "/"], function(){
 		ini_set("soap.wsdl_cache_enabled", 0);
 
 		list($usr, $pass) = explode("|", getenv("BANK_WS_CREDENTIALS"));
+		$data = Request::all();
 		$type = $data['type'];
 		unset($data['type']);
-		$data = Request::all();
         $data['date'] = date("Y-m-d");
         $data['string'] = "blablabla";
         $data['user'] = $usr;
