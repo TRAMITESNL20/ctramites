10e222324849bc572a51b2a6f9a070a9651891d3
jaimexcastillo <jaime.castilloc@nuevoleon.gob.mx>
2021-04-19 17:42:58 -0500

    acomodo del label para el qr

--- resources/views/pdf/formatoDeclaracionISR.blade.php
+++ resources/views/pdf/formatoDeclaracionISR.blade.php
@@ -273,8 +273,8 @@
 			@endif
 		</tbody>
 	</table>
-	<div class="footer">
-		<table style="width: 100%; margin-top:0px; text-align:center;" >
+	<div class="footer"   >
+		<table style="width: 100%; margin-top:0px; float: right;" border="0" >
 			<tr class="">
 				<td class=""></td>
 				<td class=""></td>
@@ -283,7 +283,7 @@
 			<tr class="">
 				<td class="border_bottom  columna" style="text-align:left	">{{ $user}}   </td>
 				<td class="border_bottom columna"></td>
-				<td class="border_bottom columna value text-left">Codigo QR Oficial</td>
+				<td  class="border_bottom columna value text-right" style="width:150px">Codigo QR Oficial</td>
 			</tr>
 			<tr class="datos-enajenante">
 				<td  colspan="3"><div style="height:26px;"></div></td>
