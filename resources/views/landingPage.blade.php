60a8f1df4bdf070e27a525ef8da2092a8ff37916
jaimexcastillo <jaime22acc@outlook.com>
2021-05-07 13:34:47 -0500

    landing pge conf

---
+++ resources/views/landingPage.blade.php
@@ -0,0 +1,26 @@
+ 
+    <div class="w-100 fixed " style="top:0;position:fixed;height:70px">
+            <div class="navbar  w-100 " style="background-color:#3ba3fb">
+                <img src="images/logo.svg" style="color:white" width="200" height="50">
+                <a class="text-white" href="#">VENTANILLA UNICA DIGITAL</a>
+                <div style="right:0" class="text-white">
+                    captura tu folio de segumiento<input  class="input w-25 pl-4" style="background-color:white" type="text">
+                </div>
+            </div>
+            
+    </div>
+
+<div class="content">
+    <!-- <div class="container"> -->
+        <div class="pl-10">
+            <h2>Accede a trámites y servicios</h2>
+            <h2>desde cualquier lugar</h2>
+        </div>
+
+    <!-- </div> -->
+    <div>
+        <div class="">2</div>
+        <div class="card w-30">1</div>
+        <div class="card w-30">3</div>
+    </div>
+</div>
