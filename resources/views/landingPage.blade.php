7a23455fa20c3eb4d04fa88de020f62aabca496f
jaimexcastillo <jaime.castilloc@nuevoleon.gob.mx>
2021-05-07 19:56:33 -0500

    html template

--- resources/views/landingPage.blade.php
+++ resources/views/landingPage.blade.php
@@ -1,26 +1,100 @@
- 
+
+
     <div class="w-100 fixed " style="top:0;position:fixed;height:70px">
-            <div class="navbar  w-100 " style="background-color:#3ba3fb">
+            <div class="  w-100 " style="background-color:#3ba3fb;position: relative;display: flex;flex-wrap: wrap;align-items: center;padding: 0.5rem 1rem;">
                 <img src="images/logo.svg" style="color:white" width="200" height="50">
                 <a class="text-white" href="#">VENTANILLA UNICA DIGITAL</a>
-                <div style="right:0" class="text-white">
-                    captura tu folio de segumiento<input  class="input w-25 pl-4" style="background-color:white" type="text">
+                <div style="right:0;margin-left:auto" class="text-white">
+                    <span class="text-uppercase text-white" style="font-size:12px">captura tu folio de segumiento</span><input  class="input w-25 pl-4" style="background-color:white;margin-left:20px" type="text">
+                    <span class="text-white px-12" style="font-size:8px">OBTEN TU CFDI</span>
+                    <i class="fas fa-search text-white"></i>
                 </div>
+
             </div>
             
     </div>
+    <div class="content ">
+        <div class="d-flex flex-column-fluid">
+            <div class="dashboard pt-0">
+        <!-- <div class="container"> -->
+                <div class="pl-40 pb-20">
+                    <h2 class>Accede a trámites y servicios</h2>
+                    <h2>desde cualquier lugar</h2>
 
-<div class="content">
-    <!-- <div class="container"> -->
-        <div class="pl-10">
-            <h2>Accede a trámites y servicios</h2>
-            <h2>desde cualquier lugar</h2>
+                    <div class="btn-group" style="width:35%">
+                        <input type="search" class="ds-input form-control w-45"  
+                        placeholder="¿cual trámite deseas realizar?" 
+                        aria-label="¿cual trámite deseas realizar?" 
+                        autocomplete="off" 
+                        spellcheck="false" 
+                        role="combobox" 
+                        aria-autocomplete="list" 
+                        aria-expanded="false" 
+                        aria-owns="algolia-autocomplete-listbox-0" 
+                        dir="auto" 
+                        style="position: relative; vertical-align: top;width:100% !important;border-color:#2699fb;color:#2699fb;height: unset;border-radius:unset"
+                        >
+                        <button type="button" id="btnWizard" data-wizard-type="action-next" 
+                                class="btn btn-primary font-weight-bolder text-uppercase px-9 py-4" >
+                        <i class="fas fa-arrow-right"></i>
+                        </button>
+                    </div>
+                </div>
+
+        <!-- </div> -->
+                <div class="d-flex w-100" style="background-color:#f1f8fe" >
+                    <div class="col-4 px-25">
+                        <h4>Actas, inexistencias y copias certificadas</h4>
+                        <ul style="font-size:10px">
+                            <li class="p-2">Copia certificada del acta de nacimiento</li>
+                            <li class="p-2">Copia certificada del acta de matrimonio</li>
+                            <li class="p-2">Copia certificada del acta de defuncion</li>
+                            <li class="p-2">Constancia de inexistencia de matrimonio</li>
+                            <li class="p-2">Constancia de inexistencia de defuncion</li>
+                            <li class="p-2">Constancia de inexistencia de nacimiento</li>
+                            <li class="p-2">Juicios de aclaracioon y correcion de actas</li>
+                        </ul>
+                    </div>
+                    <div class="col-4 px-25">
+                        <h4>Pago de impouestos y contribuciones</h4>
+                        <ul style="font-size:10px">
+                            <li class="p-2">Pago de impuesto sobre nomina</li>
+                            <li class="p-2">Pago de impuesto a las tarifas efectivamente cobradas por las empresas de redes de transporte</li>
+                            <li class="p-2">Pago de impuesto sobre automoviles nuevos</li>
+                            <li class="p-2">Pago de impuestopor juegos con apuestas y obtencion de premios</li>
+                            <li class="p-2">Pago de impuesto sobre hospedaje</li>
+                            <li class="p-2">Pago de impuesto predial</li>
+                            <li class="p-2">Pago de 5% de ISR regimen intermedio</li>
+                            <li class="p-2">Declaracion del regimen de incorporacion fiscal</li>
+                            <li class="p-2">Registro estatal de contribuyentes</li>
+                            <li class="p-2">Consulta al registro estatal de contribuyentes</li>
+                        </ul>
+                    </div>
+                    <div class="col-4 px-25">
+                        <h4>Educacion</h4>
+                        <ul style="font-size:10px">
+                            <li class="p-2">Expedicion de duplicados de educacion basica</li>
+                        </ul>
+
+                        <h4>Control vehicular</h4>
+                        <ul style="font-size:10px">
+                            <li class="p-2">Pago de regrendo vehicular</li>
+                            <li class="p-2">Renovacion de licencia de conducir</li>
+                        </ul>
+
+                        <h4>Parques y vida silvestre</h4>
+                        <ul style="font-size:10px">
+                            <li class="p-2">Licencia de caza</li>
+                            <li class="p-2">Expedicion de cintillos</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
-
-    <!-- </div> -->
-    <div>
-        <div class="">2</div>
-        <div class="card w-30">1</div>
-        <div class="card w-30">3</div>
     </div>
-</div>
+
+    <footer class="bg-light align-items-center px-3" style="position: fixed; bottom: 0px; width: 100%;backgorund-color: #a6e0fd">
+        <div class="col text-center" style="color:#56affb">
+            <strong class="mr-2" style="color:#56affb">2021</strong> <strong class="mr-2" style="color:#56affb">Aviso de Privacidad</strong> <strong style="color:#56affb">Contacto</strong>
+        </div>
+    </footer>
