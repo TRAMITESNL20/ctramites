b18d4d3a785eb039a72eb0c2959c71e959ee571c
jaimexcastillo <jaime22acc@outlook.com>
2021-05-13 11:06:02 -0500

    usuarios ahora es una lista

--- resources/js/components/Usuarios.vue
+++ resources/js/components/Usuarios.vue
@@ -35,32 +35,90 @@
             </div>
 
             <!-- lista de usuarios -->
-            <div class="row ">            
-                <div class="col-md-4" v-for="usuario in filteredHelper" :key="usuario.id"> 
-                    <div class="card " style="height: 300px;width: 280px;">
-                            <div class="card-body"  style="position:relative">
-                                <div class="text-center">
-                                    <a type="button" ><img src="images/avatar6.png" class="mt-8" style="max-width: 70px;border-radius: 50%" ></img></a>
-                                </div>
-                            <div class="text-center pt-2">
-                                <div>
-                                        <span style="font-style: oblique;font-size: 15px">{{usuario.name }} {{usuario.fathers_surname}}  {{usuario.mothers_surname}}</span>
-                                </div>
-                                <div>
-                                    <span class="form-text text-muted">{{usuario.email}} </span>
-                                </div>
-                            </div>
-                                <div class="text-center" style="bottom:25px; position:absolute; padding-left: inherit;">
-                                    <a :href="'mailto:'+ usuario.email " class="p-3 m-2" type="button" style="background-color: #d7e4f4;border-radius: 50%" ><i style="color: #347ab6" class="fas flaticon-multimedia"></i></a>
-                                    <a class="p-3 m-2" type="button" style="background-color: #d2dae0;border-radius: 50%" ><i style="color: #25457e" class="fas fas fa-phone-alt"></i></a>
-                                    <a class="p-3 m-2" type="button" style="background-color: #ddd5ea;border-radius: 50%" ><i style="color: #3f2174" class="fas flaticon2-arrow"></i></a>
-                                </div>
-                            </div>
-                    </div>
-                </div>
             
+              <div>
+                <div class="w-100" v-for="(usuario, index) in usuarios" >
+                    <div class="card list-item card-custom mb-5 col-lg-12" style="background-color: rgb(217, 222, 226) !important" >
+                        <div class="d-flex mobile-lista-multiple align-items-center mb-3">
+                        <!---->
+                        <div class="symbol symbol-75 mr-5">
+                            <span class="symbol-label font-size-h2 font-weight-bold bg-dark-o-30"> {{  usuario.name.substr(0,1) }} {{ usuario.fathers_surname.substr(0,1 )  }}</span>
+                        </div>
+                        <div class="mr-auto espace-checkbox-text desktop-agrupacion-width" style="width: 70%">
+                            <h4 class="ml-3 text-uppercase text-truncate">
+                            <strong>{{ usuario.name }} {{ usuario.mothers_surname}} {{usuario.fathers_surname}}</strong>
+                            </h4>
+                            <h5 class="ml-3">
+                            <span style="font-weight: normal" >
+                                <strong> {{ usuario.roles.description }}   </strong> - {{usuario.curp}}
+                            </span>
+                            </h5>
+                        </div>
+                        <div class="my-lg-0 my-1">
+                            <!---->
+                            <div  class="input-group-append">
+                            <!-- alerta de que no puedes descargar el documento -->
+<!-- 
+                            <button 
+                                class="btn btn-danger disabled" 
+                                style="border-top-right-radius: 0px;border-bottom-right-radius: 0px;font-size: 7px;border-radius: 1 1 0 0 !important;" 
+                                data-toggle="tooltip" 
+                                data-placement="top" 
+                                data-original-title="No es posbile descargar este archivo al no contar con el documento del CALCULO DEL ISR CONFORME AL 126 LISR O COMPROBANTE DE LA EXENCIÓN ">
+                                    <i class="text-white fa fa-question-circle"></i>
+                            </button> -->
+
+                                <!-- boton de descarga  -->
+                                <a  href="mailto:usuario.email" target="_blank"  class="btn btn-primary font-weight-bolder text-uppercase text-white mr-5" >Enviar Correo </a>
+                                <!-- boton de collapse -->
+                                <button
+                                class="btn btn-secondary"
+                                type="button"
+                                data-toggle="collapse"
+                                :data-target="`#collapse-${index}`" 
+                                aria-expanded="false"
+                                :aria-controls="`collapse-${index}`"
+                                >
+                                <i class="fas fa-chevron-down p-0"></i>
+                                </button>
+                            </div>
+                            <!---->
+                        </div>
+                        </div>
+                        <!-- ----- COLAPSABLE ---- -->
+                        <div  :id="`collapse-${index}`" class="collapse" style="" >
+                        <div class="card list-item card-custom gutter-b col-lg-12" >
+                            <div class="card-body p-0">
+                            <div class="d-flex">
+                                <div class="flex-grow-1">
+                                <div class="d-flex align-items-center justify-content-between flex-wrap" >
+                                    <!---->
+                                    <div class="mr-auto" style="width: 100%; height:350px; overflow:scroll">
+                                    <a class="d-flex text-dark over-primary font-size-h5 font-weight-bold mr-3 flex-column" ><!---->
+                                        <!---->
+
+
+                                    </a>
+                                    </div>
+                            
+                                </div>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+                        
+                        </div>
+
+                    </div>
+                <!---->
+                </div>
 
             </div>
+
+
+
+
+
 
             <!-- paginado -->
             <div  class="row">
