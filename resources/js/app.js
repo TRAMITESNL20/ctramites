fb77b6af92577477357a0869d35c59826c1d636c
Raymundo Salazar <raymundo.salser@hotmail.com>
2021-05-14 14:33:34 -0500

    validar si existe tabla

--- resources/js/app.js
+++ resources/js/app.js
@@ -166,15 +166,17 @@ const app = new Vue({
     el: '#app',
     updated(){
         console.log('se carga el js, sigue ...');
-        var height = $(".table")[0].clientHeight;
-        $(".border-table-left").css( "width" ,'12%');
-        $(".border-table-right").css( "width" ,'12%');
-        $(".border-table-left").css( "height" ,  height+"px" );
-        $(".border-table-right").css( "height" ,  height+"px" );
-        $('#scrollDiv').scroll( function() {    
-            ( $('#scrollDiv').scrollLeft() == ($('#scrollDiv table').width() - $('#scrollDiv').width())) ?  $('#gradientBackgroundRight').hide() : $('#gradientBackgroundRight').show();
+        if($(".table").length > 0){
+            var height = $(".table")[0].clientHeight;
+            $(".border-table-left").css( "width" ,'12%');
+            $(".border-table-right").css( "width" ,'12%');
+            $(".border-table-left").css( "height" ,  height+"px" );
+            $(".border-table-right").css( "height" ,  height+"px" );
+            $('#scrollDiv').scroll( function() {    
+                ( $('#scrollDiv').scrollLeft() == ($('#scrollDiv table').width() - $('#scrollDiv').width())) ?  $('#gradientBackgroundRight').hide() : $('#gradientBackgroundRight').show();
 
-            ( $('#scrollDiv').scrollLeft() > 0) ? $('.border-table-left').show() : $('#gradientBackgroundLeft').hide();
-        });
+                ( $('#scrollDiv').scrollLeft() > 0) ? $('.border-table-left').show() : $('#gradientBackgroundLeft').hide();
+            });
+        }
     }
 });
