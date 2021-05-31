21ba0c939e39c9670fcab4f478ae11e7be31172a
alberto <albon_marvel@hotmail.com>
2021-05-31 14:21:58 -0500

    edicion de vendedores

--- resources/js/components/AvisoEnajenacionModule/ModalAvisoEnajenacionComponent.vue
+++ resources/js/components/AvisoEnajenacionModule/ModalAvisoEnajenacionComponent.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
-        <b-button style="color:white" @click="openModal()" >
-          <i style="color:white"></i> 
-          {{ config.labelBtnAddItem || 'Agregar' }}
+        <b-button style="color:white" @click="openModal()"   :class="classBtn">
+          <i style="color:white" :class="btnIcon"></i> 
+          {{ labelBtnAddItem  }}
         </b-button>
         <b-modal size="lg" :id="idModal" ref="modal" :title="titleModal"  @ok="handleOk" @hidden="resetModal" 
             :ok-title = "btnOkLabel"   no-close-on-backdrop :ok-disabled="formaIvalid">
@@ -17,10 +17,10 @@
                         </v-expansion-panel-header>
                         <v-expansion-panel-content>
                             <formulario-datos-personales-component 
-                                @estadoFormulario="estadoFormulario">
+                                @estadoFormulario="estadoFormulario" :datosPersonales="datosPersonales">
                             </formulario-datos-personales-component>
                             <formulario-datos-aviso-enajenacion-component 
-                                @estadoFormulario="estadoFormularioPorcentajes">
+                                @estadoFormulario="estadoFormularioPorcentajes" :datosPorcentajes="datosPorcentajes" :config="config">
                             </formulario-datos-aviso-enajenacion-component>  
                         </v-expansion-panel-content>
                     </v-expansion-panel>
@@ -33,7 +33,7 @@
                             </template>
                         </v-expansion-panel-header>
                         <v-expansion-panel-content>
-                            <formulario-direccion-notificacion-component @estadoFormulario="estadoFormularioDireccion"></formulario-direccion-notificacion-component>
+                            <formulario-direccion-notificacion-component @estadoFormulario="estadoFormularioDireccion" :datosDireccion="datosDireccion"></formulario-direccion-notificacion-component>
                         </v-expansion-panel-content>
                     </v-expansion-panel>
                 </v-expansion-panels>
@@ -45,12 +45,15 @@
 <script>
 import { uuid } from 'vue-uuid';
 export default {
-    props: [ 'config'],
+    props: [ 'config', 'item', 'indexItem'],
     data(){
         return {
-            idModal:'mi_id' + this.config.name || uuid.v4() ,
+            idModal:'add_id' + this.config.name || uuid.v4() ,
             titleModal: this.config.titleModal || 'Agregar',
+            labelBtnAddItem: this.labelBtnAddItem || 'Agregar',
             btnOkLabel:'Guardar',
+            classBtn : "btn bg-success w-80 mb-4",
+            btnIcon: "la la-plus",
             form: {
 
             },
@@ -70,19 +73,43 @@ export default {
         }
     },
     mounted() {
-        console.log("el MODAL AVISO");
+        if(this.item){
+            this.idModal =  'edit_id' + this.config.name + "-index-" + this.indexItem || uuid.v4();
+            this.titleModal = this.config.titleModalEdit || 'Editar';
+            this.labelBtnAddItem = '';
+            this.classBtn = "btn-info";
+            this.btnIcon = "la la-pencil"; 
+        }
+        
     },
     methods: {
-        openModal(){
+        openModal(){      
+            if(this.item){
+                this.datosPersonales = Object.assign({}, this.item.datosPersonales);
+                this.datosPorcentajes = Object.assign({}, this.item.datosPorcentajes);
+                this.datosDireccion = Object.assign({}, this.item.datosDireccion);
+            } else {
+                this.datosPersonales = {};
+                this.datosPorcentajes = {}; 
+            }
             this.$bvModal.show(this.idModal);
         },
 
         handleOk(bvModalEvt){
             bvModalEvt.preventDefault();
-            let data = this.datosPorcentajes;
+            let data = {};
             data.datosPersonales = this.datosPersonales;
+            data.datosPorcentajes = this.datosPorcentajes;
 
-            this.$emit('addItem', data);
+            if(this.item){
+              let response = {
+                item:data, 
+                index:this.indexItem
+              }
+              this.$emit('editItem', response);
+            }else{
+              this.$emit('addItem', data);
+            }
             this.$nextTick(() => {
                 this.$bvModal.hide(this.idModal)
             })
@@ -95,14 +122,13 @@ export default {
         estadoFormulario(validationForm){
             this.formularioDatosPersonalesInvalid = validationForm.$invalid;
             if( !this.formularioDatosPersonalesInvalid ){
-                this.datosPersonales = validationForm.form.$model;
+                this.datosPersonales = validationForm.form.$model ;
             } else {
                 this.datosPersonales = {};
             }
         },
 
         estadoFormularioPorcentajes(validationForm){
-                         console.log(JSON.parse( JSON.stringify(validationForm.form.$model) ))
             this.formularioDatosPorsentajesInvalid = validationForm.$invalid;
             if( !this.formularioDatosPorsentajesInvalid ){
                 this.datosPorcentajes = validationForm.form.$model;
--- resources/js/components/TableComponent.vue
+++ resources/js/components/TableComponent.vue
@@ -4,6 +4,9 @@
             <div class="table-responsive">
                 <b-table responsive striped hover :items="data" :fields="fields">
                     <template #cell(tipoPersona)="data">
+                        <button @click="eliminar(data.index)">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
                         {{ data.item.tipoPersona == 'pf' ? 'Persona Física' : 'Persona Moral' }}
                     </template>
                     <template #cell(datosPersonales)="data">
@@ -26,14 +29,18 @@
                     </template>
                     <template #cell(actions)="data">
                         <span>
-                            hola mundo
+                            <modal-aviso-enajenacion-component 
+                            @editItem="editItem" :config="config" :item="data.item" :indexItem="data.index">
+                                
+                            </modal-aviso-enajenacion-component>
                         </span>
                     </template>
                 </b-table>             
             </div>
         </b-col>
-        <b-col  cols="12" >
-            <modal-aviso-enajenacion-component @addItem="addItem" :config="config"></modal-aviso-enajenacion-component>
+        <b-col  cols="12" v-if=" config.porcentajePropiedadAsignado  != 100 ||  config.porcentajeUsufructoAsignado != 100">
+            <modal-aviso-enajenacion-component @addItem="addItem" :config="config">
+            </modal-aviso-enajenacion-component>
        </b-col>
     </div>
 </template>
@@ -41,7 +48,7 @@
 <script>
 import avisoEnajenacionService from '../services/avisoEnajenacion.services.js';
 export default {
-    props: [ 'type', 'config'],
+    props: [ 'type', 'config', 'itemsPre'],
     data(){
         return {
             isVendedor:false,
@@ -55,19 +62,72 @@ export default {
                 { key: 'actions', class:'text-center', label:'Acciones'}
 
             ],
-            data:[]
+            data:[],
+            porcentajePropiedadAsignado: 0,
+            porcentajeUsufructoAsignado:0
         }
     },
     mounted() {
+        if(this.itemsPre && this.itemsPre.length > 0) {
+            this.items = this.itemsPre;
+        }
+        this.mapearDatos();
     },
     methods: {
         addItem(data){
             this.items.push( data );
+            this.mapearDatos();
+        },
+
+        editItem( res ){
+            this.items[res.index] = res.item;
+            this.mapearDatos();
+        },
+
+        mapearDatos(){
             this.data = this.items.map( item => {
                 item.tipoPersona = item.datosPersonales.tipoPersona;
+                item.porcentajePropiedad =  item.datosPorcentajes.porcentajePropiedad;
+                item.porcentajeUsufructo = item.datosPorcentajes.porcentajeUsufructo;
+                item.porcentajeVenta = item.datosPorcentajes.porcentajeVenta;
                 return item;
-            })
+            }); 
+            this.calcularPorcentajePropiedadAndUsufructo();           
+        },
+
+        calcularPorcentajePropiedadAndUsufructo(){
+            let totalPorcentajePropiedad = 0;
+            let totalPorcentajeUsufructo = 0;
+            if(this.items && this.items.length > 0){
+                this.items.forEach( item  => {
+                    totalPorcentajePropiedad = Number(Number(Number(totalPorcentajePropiedad) + Number(item.datosPorcentajes.porcentajePropiedad)).toFixed(this.$const.PRECISION));
+                    totalPorcentajeUsufructo = Number(Number(Number(totalPorcentajeUsufructo) + Number(item.datosPorcentajes.porcentajeUsufructo)).toFixed(this.$const.PRECISION));
+                });
+            }
+
+            this.config.porcentajePropiedadAsignado = totalPorcentajePropiedad;
+            this.config.porcentajeUsufructoAsignado = totalPorcentajeUsufructo;
+
+            this.notify();
+        },
+
+        notify(){
+            let data = {
+                items:this.items,
+                config:this.config
+            }
+            this.$emit('listaItems', data);
+        },
+
+        eliminar(indice){
+            this.items.splice(indice,1);
+            this.mapearDatos();
         }
+
+
+
+
+
     },
 
 }
