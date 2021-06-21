120d7634de581f0ec8cde02fb2520d712470965d
jaimexcastillo <jaime.castilloc@nuevoleon.gob.mx>
2021-06-21 16:44:48 -0500

    se recorren los informativos falta mostrar en listado

--- resources/js/components/tiposElementos/FirmaElectronicaComponent.vue
+++ resources/js/components/tiposElementos/FirmaElectronicaComponent.vue
@@ -31,9 +31,8 @@
 		},
 		mounted() {
 			console.log('=============');
-			console.log(this.usuario.tramite_id);
+			console.log(this.usuario.solicitudes);
 			window.addEventListener("message", this.messageEvt, false);
-			console.log(this.usuario);
 			if(this.usuario.tramite_id == process.env.TRAMITE_5_ISR || this.usuario.tramite_id == process.env.TRAMITE_AVISO ){
 				this.usuario.solicitudes.map((solicitud, ind) => {
 				// console.log(solicitud);
@@ -79,14 +78,15 @@
 				this.urlFirmado.push( `${process.env.INSUMOS_DOCS_HOSTNAME}/firmas//${this.usuario.tramite_id + "_" +  this.usuario.solicitudes[0].id}/${solicitud.id}_${this.usuario.tramite_id}_${this.usuario.solicitudes[0].id}_firmado.pdf` );
 				});
 			}else if(this.usuario.tramite_id == 8 /*process.env.TRAMITE_INFORMATIVO*/){
-				this.usuario.solicitudes.map((solicitud, ind) => {
-					this.getDocumentCatastro(solicitud)
-				});
+				for (let i = 0; i < this.usuario.solicitudes.length; i++) {
+					this.usuario.solicitudes[i].info.campos['Resultados Informativo Valor Catastral'].map(( solicitud, indSolicitud) => {
+						console.log('aqi');
+						this.getDocumentCatastro(solicitud, indSolicitud, i);
+					});					
+				}
+
 			}	
 			console.log('====-----====');
-			console.log(this.usuario.tramite_id);
-			console.log(process.env);
-			console.log(this.usuario.tramite_id == process.env.TRAMITE_INFORMATIVO);
 			this.rfc = this.user.rfc;
 			this.accesToken();
 			this.encodeData();
@@ -195,8 +195,8 @@
 					
 				}
 			},
-			getDocumentCatastro(solicitud , tramiteId){
-				console.log( JSON.stringify(solicitud) );
+			async getDocumentCatastro(solicitud , tramiteInd, indTramite ){
+				// console.log( JSON.stringify(solicitud) );
 				var adquirientes = [];
 				var vendedores =[];
 				var tipoTramite= '';
@@ -264,40 +264,45 @@
 									});
 					}
 				}
+				console.log( 'aca');
 				var dataCatastro = [
 					{
 						"json":{
-							"expedientecatastral": solicitud.info.campos['No. EXP. CATASTRAL'] ? solicitud.info.campos['Resultados Informativo Valor Catastral'][0].expediente_catastral : '',
+							"expedientecatastral": this.usuario.tramite_id == process.env.TRAMITE_AVISO ? solicitud.info.campos['No. EXP. CATASTRAL'] ? solicitud.info.campos['Resultados Informativo Valor Catastral'][tramiteInd].expediente_catastral  : '' : solicitud.expediente_catastral ,
 							"pktramite": tipoTramite ,
 							"pknotaria": this.user.notary.id ?? '',
 							"estadonotaria": "19",
 							"foliopago":123456,
 							"fechapago": this.usuario.tramite_id == process.env.TRAMITE_AVISO ? new Date().toISOString().slice(0, 10) : new Date().toISOString().slice(0, 10) ,
-							"montopago": this.usuario.tramite_id == process.env.TRAMITE_AVISO ? '' : solicitud.info.costo_final ?? '' ,
+							"montopago": this.usuario.tramite_id == process.env.TRAMITE_AVISO ? solicitud.info.costo_final : '1231' ? this.usuario.solicitudes[indTramite].info.costo_final : '1231',
 							"tipoventa": 'terreno ?',
-							"isai": solicitud.info.campos.ISAI ?? 'SIATEST',
+							"isai": this.usuario.tramite_id == process.env.TRAMITE_AVISO ? solicitud.info.campos.ISAI : 'SIATEST' ? this.usuario.solicitudes[indTramite].info.campos.ISAI : 'ISAIINFORMATIVO',
 							"fechaprot": this.usuario.tramite_id == process.env.TRAMITE_AVISO ?  solicitud.info.campos['Fecha de protocolización'] : '',
 							"fechafirma": new Date().toISOString().slice(0, 10),
-							"escriturapub": solicitud.info.campos['Número de Escritura Pública'] ?? '12345',
-							"actafprot": solicitud.info.campos['Acta Fuera Protocolo'],
+							"escriturapub": this.usuario.tramite_id == process.env.TRAMITE_AVISO ? solicitud.info.campos['Número de Escritura Pública'] : '12345' ?  this.usuario.solicitudes[indTramite].info.campos['Número de Escritura Pública'] : '54321' ,
+							"actafprot":  this.usuario.tramite_id == process.env.TRAMITE_AVISO ?  solicitud.info.campos['Acta Fuera Protocolo'] : '' ? this.usuario.solicitudes[indTramite].info.campos['Acta Fuera Protocolo'] : '' ,
 							"avaluo":12345,
 							"operacion": this.usuario.tramite_id == process.env.TRAMITE_AVISO ? 0 : 12345,
 							"motivooperacion":"motivo",
 							"fiscal":12345678,
 							"folioforma":"12345",
-							"descripcion_predio": solicitud.info.campos['Anexo descripción'] ?? 'descripcionTest',
+							"descripcion_predio": this.usuario.tramite_id == process.env.TRAMITE_AVISO ? solicitud.info.campos['Anexo descripción'] : 'descripcionTest' ? this.usuario.solicitudes[indTramite].info.campos['Anexo descripcion'] : '',
 							"adquirientes": adquirientes,
 							"vendedores": vendedores,
 						}
 					}
 				];
-
 				console.log(dataCatastro);
 				var url =  process.env.TESORERIA_HOSTNAME + "/registro-catastro";
-				fetch(url, { 'method': 'POST', 'body' : JSON.stringify(dataCatastro[0]) } )
-				 .then(response => console.log(response.URL))
+				await fetch(url, { 'method': 'POST', 'body' : JSON.stringify(dataCatastro[0]) } )
+				 .then(res =>  res.json())
+				 .then(res => {
+					 var responseJson = JSON.parse(res.response.replace('\ufeff', ''));
+					 console.log(responseJson.URL);
+					 this.firma = responseJson.URL;
+				 })
 				 .catch( error => console.log(error));
-
+				
 			},
 		},
 	}
