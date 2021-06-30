a6e89990224cdd1adb6db842283864eeca8c6d29
alberto <albon_marvel@hotmail.com>
2021-06-30 17:43:08 -0500

    clase que ejecuta la actualizacion

---
+++ app/Custom/AdminCostosTramites.php
@@ -0,0 +1,50 @@
+<?php 
+namespace App\Custom;
+
+use App\Custom\Tramite5ISR;
+use Illuminate\Support\Facades\Log;
+
+class AdminCostosTramites 
+{
+
+    public function __construct(Tramite5ISR $tramite5ISR){
+		$this->tramite5ISR = $tramite5ISR;
+    }
+
+
+    public function updateISR($tramite){
+        Log::info('Obtener costos total actual');
+        $this->tramite5ISR->setTramite( $tramite );
+        $totalActual =  $this->tramite5ISR->getTotalActualISR();
+        if(isset($totalActual)){
+            Log::info('Importe total actual:' . $totalActual );
+            Log::info('Obtener campos para calcular costo nuevo ');
+            $params = $this->tramite5ISR->getParamsParaCosto();
+            if( count($params) > 0 ){
+                Log::info('**********Datos para deteterminar el impuesto**********');
+                Log::info( print_r ($params, true));
+                Log::info('Obtener costo nuevo');
+                $detalle =  $this->tramite5ISR->calcularTotal($params);
+                $detalleObj = json_decode( $detalle, true, 512);
+                Log::info("Importe nuevo:" . $detalleObj['Salidas']["Importe total"] );
+                Log::info("Necesario actualizar :" . ($totalActual != $detalleObj['Salidas']["Importe total"] ? 'SI' : 'NO' )  );
+                Log::info('Detalle:');
+                Log::info( print_r ($detalleObj, true));
+                if( $totalActual != $detalleObj['Salidas']["Importe total"] ){
+                	return $this->tramite5ISR->updateDetalle();
+                } else {
+                	return false;
+                }
+                Log::info('Fin update');
+            } else {
+                Log::error('##########No hay parametros para consultar detalle');  
+                return false;
+            }
+            
+        } else {
+            Log::error('No tiene Importe total');
+            return false;
+        }        
+    }
+
+}
