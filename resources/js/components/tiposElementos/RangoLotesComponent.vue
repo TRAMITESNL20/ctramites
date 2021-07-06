54ee796a922c72a2f9d90c47c2986abdb1d25a7b
jaimexcastillo <jaime.castilloc@nuevoleon.gob.mx>
2021-07-06 17:30:38 -0500

    formato de tabla

--- resources/js/components/tiposElementos/RangoLotesComponent.vue
+++ resources/js/components/tiposElementos/RangoLotesComponent.vue
@@ -1,25 +1,27 @@
 <template>
   <div>
-      <table class="table  table-bordered">
+      <table v-if="cantidadManzanas > 0" class="table  table-bordered">
+          <thead>
+              <tr>
+                  <td></td>
+                  <td>Lote inicial</td>
+                  <td>Lote Final</td>
+                  <td>Lote Unico</td>
+              </tr>
+          </thead>
           <tbody>
               <tr v-for="(manzana, index) in  cantidadManzanas" :key="index">
                   <td>
                       Manzana #{{index + 1}}
                   </td>
                   <td>
-                      Lote Inicial
+                        <input type="text" class="form-control" placeholder="000" >
                   </td>
                   <td>
                         <input type="text" class="form-control" placeholder="000" >
                   </td>
-                  <td>
-                       Lote Final 
-                  </td>
-                  <td>
-                        <input type="text" class="form-control" placeholder="000" >
-                  </td>
-                  <td>
-                      <input type="checkbox" name="" id="">
+                  <td class="text-center">
+                      <input class="text-center" type="checkbox" name="" id="">
                   </td>
               </tr>
           </tbody>
