8a61754bb9ce176f2de08414ddba3ff24be92f47
FECD <cerdaduran@gmail.com>
2021-07-13 16:21:58 -0500

    Implementacion RC Nacimiento, Defuncion y Matrimonio

--- resources/js/app.js
+++ resources/js/app.js
@@ -69,7 +69,7 @@ Vue.component("modal-document-component" , () => import( /* webpackChunkName: "j
 Vue.component("card-porfile-component" , () => import( /* webpackChunkName: "js/components/tiposElementos/results-component" */ './components/generales/CardPorfileComponent.vue' ));
 
 Vue.component("btn-guardar-tramite-parent" , () => import( /* webpackChunkName: "js/components/generales/btn-guardar-tramite-parent" */ './components/generales/BtnGuardarTramiteParent.vue' ));
-
+Vue.component("btn-busqueda-acta-component", () => import(/* webpackChunkName: "js/components/generales/btn-busqueda-acta-component" */ './components/generales/BtnBusquedaActaComponent.vue'));
 Vue.component("btn-guardar-tramite-component" , () => import( /* webpackChunkName: "js/components/generales/btn-guardar-tramite-component" */ './components/generales/BtnGuardarTramiteComponent.vue' ));
 Vue.component("fecha-component" , () => import( /* webpackChunkName: "js/components/tiposElementos/fecha-component" */ './components/tiposElementos/FechaComponent.vue' ));
 Vue.component("enajenantes-component" , () => import( /* webpackChunkName: "js/components/enajenantes-component" */ './components/EnajenantesComponente.vue' ));
