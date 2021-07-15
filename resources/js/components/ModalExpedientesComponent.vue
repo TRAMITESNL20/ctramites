54dd36c5a7ebcb0b06f67f8c291d3338f86a2a43
alberto <albon_marvel@hotmail.com>
2021-07-15 13:07:13 -0500

    quitar notaria hardcore

--- resources/js/components/ModalExpedientesComponent.vue
+++ resources/js/components/ModalExpedientesComponent.vue
@@ -281,7 +281,7 @@
               let params = {
                 expediente:this.clave + nExpediente.split("-").join(""),
                 folio:this.$v.form.folio.$model,
-                id_notaria: 2//this.usuario.notary.notary_number
+                id_notaria: this.usuario.notary.notary_number
               }
               let response = await axios.get(url , {params} );
               if(response.data){
